1d792f73d996db32d205144bd69588a3f6a5b678
YOJU-e <111264176+YOJU-e@users.noreply.github.com>
2024-08-05 16:48:11 +0800

    Update ALD_ex.py

--- ALD_ex.py
+++ ALD_ex.py
@@ -16,14 +16,14 @@ import csv
 import time # 페이지 로딩을 기다리는데에 사용할 time 모듈 import
 import sqlite3
 
-def get_chrome_driver():
+def get_driver():
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+    
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=chrome_options)
     return driver
 
 def is_leap_year(year):
